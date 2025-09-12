9f97ecf371a83c0e3fe899938a26315edd2384ee
Jony Jas J <jonyjasjonyjas@gmail.com>
2025-09-12 22:09:18 +0530

    feat: plugin-service init

---
+++ backend/services/plugin-service/src/main/java/com/flowops/plugin_service/PluginServiceApplication.java
@@ -0,0 +1,13 @@
+package com.flowops.plugin_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluginServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PluginServiceApplication.class, args);
+	}
+
+}
