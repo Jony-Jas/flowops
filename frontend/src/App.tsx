52c51a8ebef5c04b10a95c280f17df69e691adee
Jony Jas J <jonyjasjonyjas@gmail.com>
2025-10-05 13:18:48 +0530

    feat: frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,21 @@
+import flowopsLogo from './assets/flowops-logo-small.png'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <a href="https://github.com/Jony-Jas/flowops" target="_blank">
+          <img src={flowopsLogo} className="logo" alt="Flowops logo" />
+        </a>
+      </div>
+      <h1>FlowOps</h1>
+      <p>
+        Coming sooon!!
+      </p>
+    </>
+  )
+}
+
+export default App
